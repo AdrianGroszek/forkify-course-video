1fa05763f1750019004f8fa5fc54bcfcb210e236
Adrian <a.grochowski17@gmail.com>
2023-09-06 12:34:52 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd4de53ef-b91e-48a7-9513-a5ee7f56148d';
