0f91d61f64e0c100e6e74f5c6e42dfeef9946573
Adrian <a.grochowski17@gmail.com>
2023-09-06 13:00:00 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -100,6 +100,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -108,6 +112,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
